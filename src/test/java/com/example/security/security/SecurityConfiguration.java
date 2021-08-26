37a36a5dd34781c4310ed62f49fc80e4f2f646b4
Matheus Felipe <65515159+mfelipemedeiros@users.noreply.github.com>
2021-08-25 22:35:48 -0300

    Code

---
+++ src/test/java/com/example/security/security/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.example.security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+    
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication().withUser("matheus").password(passwordEncoder().encode("1234")).authorities("USER");
+    }
+}
